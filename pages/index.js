b011a8f6ad2cf8ca750d9744ca8070369f4cddf6
Paulo Lucas Cerqueira <paulo.cerqueira@vilaapps.com.br>
2021-06-08 21:57:03 -0300

    adicionado header

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,40 @@
 import Head from 'next/head'
 
-export default function Home() {
+import styled from 'styled-components'
+
+function Home() {
   return (
     <div>
       <Head>
-        <title>Paulo Cerqueira</title>
+        <title>Home - Paulo Cerqueira</title>
         <meta name="description" content="Bio" />
       </Head>
+      
+      <Header>
+        <h1>Olá!</h1>
+        <h1>Me chamo Paulo</h1>
+        <p>e sou desenvolvedor tal stack, criei esse blog para compartilhar meus conhecimentos em programação e outros assuntos, espero que goste!</p>
+      </Header>
     </div>
   )
 }
+
+const Header = styled.header`
+  h1 {
+    font-family: Ubuntu;
+    font-weight: bold;
+    font-size: 96px;
+    line-height: 110px;
+    letter-spacing: -1.5px;
+  }
+
+  p {
+    font-family: Ubuntu;
+    font-style: normal;
+    font-weight: 300;
+    font-size: 24px;
+    line-height: 28px;
+  }
+`
+
+export default Home
